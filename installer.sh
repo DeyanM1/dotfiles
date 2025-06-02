7d109f721845e223f73c8fd039c19f0f0ae1e8cb
Deyan Markov <DeyanM1>
2025-06-02 19:54:32 +0200

    refactor Folder structure; added uninstaller script

--- installer.sh
+++ installer.sh
@@ -1,12 +1,18 @@
 #!/bin/zsh
 
-ln -sf /home/deyan/dotFiles/.zshrc ~/.zshrc
-ln -sf /home/deyan/dotFiles/.tmux.conf ~/.tmux.conf
-ln -sf /home/deyan/dotFiles/.wezterm.lua ~/.wezterm.lua
-ln -sf /home/deyan/dotFiles/alacritty ~/.config/alacritty
 
-ln -sf /home/deyan/dotFiles/nvim ~/.config/nvim
-ln -sf /home/deyan/dotFiles/yazi ~/.config/yazi
-ln -sf /home/deyan/dotFiles/bat ~/.config/bat
-ln -sf /home/deyan/dotFiles/.tmux ~/.tmux
+## Home Dir
+ln -sf $HOME/dotFiles/.zshrc ~/.zshrc
+ln -sf $HOME/dotFiles/.tmux.conf ~/.tmux.conf
+ln -sf $HOME/dotFiles/.wezterm.lua ~/.wezterm.lua
+ln -sf $HOME/dotFiles/.tmux ~/.tmux
+
+## dot_config
+ln -sf $HOME/dotFiles/dot_config/alacritty ~/.config/alacritty
+ln -sf $HOME/dotFiles/dot_config/nvim ~/.config/nvim
+ln -sf $HOME/dotFiles/dot_config/yazi ~/.config/yazi
+ln -sf $HOME/dotFiles/dot_condig/bat ~/.config/bat
+
+
+## OTHER
 
---
+++ uninstallers.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+delete_symlink() {
+  local TARGET="$1"
+
+  if [ -L "$TARGET" ]; then
+    echo "Deleting symbolic link: $TARGET"
+    rm "$TARGET"
+  else
+    echo "Not a symbolic link: $TARGET"
+  fi
+}
+
+## Home Dir
+delete_symlink "$HOME/.zshrc"
+delete_symlink "$HOME/.tmux.conf"
+delete_symlink "$HOME/.wezterm.lua"
+delete_symlink "$HOME/.tmux"
+
+## .config
+delete_symlink "$HOME/.config/alacritty"
+delete_symlink "$HOME/.config/nvim"
+delete_symlink "$HOME/.config/yazi"
+delete_symlink "$HOME/.config/bat"
+
